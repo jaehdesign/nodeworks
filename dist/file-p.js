0fd722f4eac35f73070f9234db1cb65056f900b2
juanan <jaehdesign@gmailcom>
2025-01-31 14:04:46 +0100

    update de nuevo files node

---
+++ dist/file-p.js
@@ -0,0 +1,4 @@
+import { join, resolve } from 'path';
+const dataPath = resolve('../data');
+console.log(dataPath); // C:\Users\Mañanas\Desktop\curso\nodeworks\test
+const file = join(dataPath, 'test.txt');
